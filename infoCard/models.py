de794d5400f4a183fff986bee2f792b733ca0be6
NuMellow <cmmukabe@gmail.com>
2018-04-30 21:02:07 +0200

    First commit

---
+++ infoCard/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Card(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+   # thumbnail = models.ImageField(upload_to=None, height_field=None
