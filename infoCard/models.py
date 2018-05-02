b1bb0e9e97b10e21c54f7df2c05bd121ea3d436f
NuMellow <cmmukabe@gmail.com>
2018-05-02 12:41:35 +0200

    Modified templates to display info cards from the database.

--- infoCard/models.py
+++ infoCard/models.py
@@ -4,3 +4,7 @@ class Card(models.Model):
     title = models.CharField(max_length = 200)
     text = models.TextField()
    # thumbnail = models.ImageField(upload_to=None, height_field=None
+
+
+    def __str__(self):
+        return self.title
--- infoCard/views.py
+++ infoCard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Card
 
 def info_list(request):
-    return render(request, 'infoCard/info_list.html', {})
+    cards = Card.objects.all()
+    return render(request, 'infoCard/info_list.html', {'cards': cards})
